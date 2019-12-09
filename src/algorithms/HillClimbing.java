d83c1069cf440c810e0396bb95a2c11b8ea8c2ba
yomna <yomna.eldawy@bosta.co>
2019-12-09 21:09:05 +0200

    core is working perfectly, other details are not yet implemented

--- src/algorithms/HillClimbing.java
+++ src/algorithms/HillClimbing.java
@@ -1,13 +1,16 @@
 package algorithms;
 
 import java.util.ArrayList;
-
+import java.util.Collections;
 import state.State;
+import state.StateComparator;
 
 public class HillClimbing implements IAlgorithm{
+	State current;
 
 	public HillClimbing(State initial) {
-		
+		this.current = initial;
+		search();
 	}
 	@Override
 	public int getRunTime() {
@@ -18,7 +21,7 @@ public class HillClimbing implements IAlgorithm{
 	@Override
 	public State getFinalState() {
 		// TODO Auto-generated method stub
-		return null;
+		return current;
 	}
 
 	@Override
@@ -33,10 +36,28 @@ public class HillClimbing implements IAlgorithm{
 		return 0;
 	}
 
+	public void search() {
+		int maxIterations = 50000;
+		int iterations = 0;
+		while (iterations++ < maxIterations && current.getStateCost() > 0) {
+			ArrayList<State> next = current.getNextStates();
+			Collections.sort(next, new StateComparator());
+			System.out.println(current.getStateCost());
+			if (next.get(0).getStateCost() >= current.getStateCost()) {
+				System.out.println("Starting over!");
+				//int index = (int) (Math.random() * next.size());
+				current = State.random();
+				// current = next.get(index);
+			}else {
+				current = next.get(0);
+			}
+		}
+		System.out.println(iterations);
+	}
+
 	@Override
 	public ArrayList<State> getPath() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
