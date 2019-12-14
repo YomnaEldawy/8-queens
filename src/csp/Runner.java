bc3deba8726155f720e3d256f9851d56b94a5d33
yomna <yomna.eldawy@bosta.co>
2019-12-14 20:51:57 +0200

    added queen icon

--- src/csp/Runner.java
+++ src/csp/Runner.java
@@ -5,6 +5,6 @@ public class Runner {
 
     public static void main(String[] args) {
         new ForwardCheckingSolver().solve8Queens();
-//        new BacktrackingSolver().solve8Queens();
+        new BacktrackingSolver().solve8Queens();
     }
 }
